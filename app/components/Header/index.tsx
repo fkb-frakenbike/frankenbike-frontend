85174599c143fea7490f72a8f4796ba94b1a87c8
Vincent Prévost <vprevost@tilli.fr>
2025-06-05 17:22:13 +0200

    feat(FKB-46): design register form

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function HeaderComponent() {
+  return (
+    <header
+      className="fixed left-0 top-0 z-50 w-full bg-transparent py-8"
+      style={{ pointerEvents: 'none' }}
+    >
+      <h1 className="font-main text-center text-3xl text-white">FKB</h1>
+    </header>
+  );
+}
--- app/components/RegisterFormComponent/index.tsx
+++ app/components/RegisterFormComponent/index.tsx
@@ -28,11 +28,11 @@ export default function RegisterFormComponent() {
       className="flex min-h-screen items-center justify-center bg-gradient-to-b from-[#2D005E] via-[#2D005E] to-[rgba(49,0,102,0.7)]"
       style={{ backgroundSize: '100% 100%', backgroundPosition: '0 0, 0 26%, 0 67%, 0 98%' }}
     >
-      <div className="mx-auto my-10 max-w-md rounded-lg bg-white p-8 shadow-md">
-        <h1 className="mb-6 text-center text-2xl font-semibold text-gray-800">Sign up</h1>
+      <div className="mx-auto my-10 max-w-md rounded-lg bg-transparent p-8">
+        <h1 className="font-other mb-6 text-center text-4xl font-semibold text-white">Sign up</h1>
         <Form action="/search" className="space-y-6">
           <div className="space-y-2">
-            <label htmlFor="firstname" className="block text-sm font-medium text-gray-700">
+            <label htmlFor="firstname" className="block text-sm font-medium text-white">
               Prénom
             </label>
             <input
@@ -42,12 +42,12 @@ export default function RegisterFormComponent() {
               value={formData.firstname}
               onChange={handleChange}
               required
-              className="w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+              className="inset-shadow-xl/20 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
             />
           </div>
 
           <div className="space-y-2">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            <label htmlFor="email" className="block text-sm font-medium text-white">
               Email
             </label>
             <input
@@ -57,12 +57,12 @@ export default function RegisterFormComponent() {
               value={formData.email}
               onChange={handleChange}
               required
-              className="w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+              className="inset-shadow-xl/20 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
             />
           </div>
 
           <div className="space-y-2">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+            <label htmlFor="password" className="block text-sm font-medium text-white">
               Mot de passe
             </label>
             <input
@@ -72,22 +72,22 @@ export default function RegisterFormComponent() {
               value={formData.password}
               onChange={handleChange}
               required
-              className="w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+              className="inset-shadow-xl/20 w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
             />
           </div>
 
           <div>
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="flex w-full justify-center rounded-full border border-white bg-transparent px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               S’inscrire
             </button>
           </div>
 
-          <div className="text-center text-sm text-gray-600">
+          <div className="text-center text-sm text-white">
             Vous avez déjà un compte?
-            <a href="/login" className="ml-1 font-medium text-indigo-600 hover:text-indigo-500">
+            <a href="/login" className="ml-1 font-medium text-white hover:text-indigo-500">
               Se connecter
             </a>
           </div>
