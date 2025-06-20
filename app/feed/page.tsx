1ced0add6020da4cce764565a8532f4da8077a67
Anael2902 <anael.toula@gmail.com>
2025-06-20 10:58:32 +0200

    FKB-58 : commit for doing acheckout

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,10 +1,12 @@
 import Feed from '../components/Feed';
+import CardComponent from '../components/CardComponent'
 
 export default function FeedPage() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-start bg-gray-100">
       <div className="w-full max-w-2xl px-4 pt-16">
         <Feed />
+        <CardComponent />
       </div>
     </div>
   );
