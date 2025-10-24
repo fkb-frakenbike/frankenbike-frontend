8415cb77218dc62b03d1fa8394dc4bf06c2b719d
Vincent Prévost <vprevost@tilli.fr>
2025-10-24 18:01:04 +0200

    refactor: simplify searchParams handling in AddComponentPage

--- app/addComponent/page.tsx
+++ app/addComponent/page.tsx
@@ -1,11 +1,9 @@
+import { PageProps } from '@/.next/types/app/page';
 import AddComponentForm from '../components/AddComponentForm';
 
-export default async function AddComponentPage({
-  searchParams,
-}: {
-  searchParams?: { projectId?: string };
-}) {
-  const projectId = searchParams?.projectId ? Number(searchParams.projectId) : undefined;
+export default async function AddComponentPage({ searchParams }: PageProps) {
+  const params = searchParams ? await searchParams : {};
+  const projectId = params?.projectId ? Number(params.projectId) : undefined;
   return (
     <div className="fkb-bg min-h-screen p-3 px-4 pt-44">
       <AddComponentForm projectId={projectId} />
